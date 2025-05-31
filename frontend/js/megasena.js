d0704abe2d66ce831db694a913b06180b81d747b
Davi Snaider <davisnaider06@gmail.com>
2025-05-31 02:34:32 -0300

    "Start front development and sweepstakes script"

---
+++ frontend/js/megasena.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Configuração com porta 3001 e endpoint correto
+    const baseUrl = 'http://localhost:3001/megasena';
+    let currentResult = null;
+
+});
